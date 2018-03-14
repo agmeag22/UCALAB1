d62a29b4e07ef64b2bc4ce30978c88afa0b830c9
Miguel Aviles <00002816@uca.edu.sv>
2018-03-14 12:11:54 -0600

    VERSION 1.0

---
+++ app/src/main/java/com/example/uca/a00002816_lab01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.uca.a00002816_lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
